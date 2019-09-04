fe1198039df7e6b542c29008b0243433d74e9850
1inguini <9647142@gmail.com>
2019-09-04 15:28:22 +0900

    metaを付け加える

--- src/Parse.hs
+++ src/Parse.hs
@@ -10,6 +10,7 @@ import           Debug.Trace
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer     as L
+import           Text.Megaparsec.Debug
 
 import           Expr
 import           RecList
@@ -71,11 +72,9 @@ identifier' = do
 
 
 var :: Parser String -> Parser ASTMeta
-var p = do
-  srcPos <- getSourcePos
+var p = meta $ do
   str <- p
-  pure ASTMeta { srcPos = srcPos
-               , ast    = ASTVar { astVar = str } }
+  pure ASTVar { astVar = str }
 
 -- 与えられた文字列を読む。後ろの空白（改行を含む）をスキップする。
 symbol :: String -> Parser String
@@ -128,39 +127,42 @@ genBinOp4OpTable str = do
 
 -- 式を読む
 exprAst :: Parser ASTMeta
-exprAst = makeExprParser term ops
+exprAst = dbg "exprAst" $
+  makeExprParser term ops
 
 -- 項を読む。項は演算子の引数になるもの。
 term :: Parser ASTMeta
-term = choice
-       [ ast2astMeta $ try anonFun
-       , try apply
-       , arg
-       , try ifAST
-       , try funDefCase
-       , fundef
-       , typeDef ]
+term = dbg "term" $
+  choice [ try anonFun
+         , try apply
+         , arg
+         , try ifAST
+         , try funDefCase
+         , fundef
+         , typeDef ]
 
 -- 関数の引数になりうるものを読む
 arg :: Parser ASTMeta
-arg = choice
-      [ ast2astMeta $ ASTDoubleLit <$> try double
-      , ast2astMeta $ ASTIntLit <$> integer
-      , ast2astMeta strLit
-      , ast2astMeta $ ASTVar <$> identifier
-      , listLit
-      , ast2astMeta $ try (parens argWithTypeSig)
-      , parens exprAst
-      , seqAST ]
+arg = dbg "arg" $
+  choice [ meta $ ASTDoubleLit <$> try double
+         , meta $ ASTIntLit <$> integer
+         , strLit
+         , var identifier
+         , listLit
+         , try (parens argWithTypeSig)
+         , parens exprAst
+         , seqAST ]
 
-ast2astMeta parseAst = do
+meta :: Parser AST -> Parser ASTMeta
+meta parseAst = do
   srcPos <- getSourcePos
   ast <- parseAst
   pure ASTMeta { srcPos = srcPos, ast = ast }
 
 -- 匿名関数を読む
-anonFun :: Parser AST
-anonFun = do
+anonFun :: Parser ASTMeta
+anonFun = dbg "anonFun" $
+  meta $ do
   params <- some identifier
   code   <- symbol "->" *> getSourcePos
   body   <- exprAst
@@ -176,24 +178,25 @@ makeGeneralType :: Int -> RecList Type
 makeGeneralType n = Elems (map (\x -> Elem ("t" ++ show x)) [0..n])
 
 -- 型注釈つきの式を読む
-astWithTypeSig :: Parser AST
-astWithTypeSig = do
+astWithTypeSig :: Parser ASTMeta
+astWithTypeSig = dbg "astWithTypeSig" $
+  meta $ do
   ast' <- exprAst
-  symbol ":"
-  sig <- typeList
+  sig <- symbol ":" *> typeList
   pure $ ASTTypeSig { astType = sig, astTypeSigVar = ast'}
 
 -- 型注釈つきの項を読む
-argWithTypeSig :: Parser AST
-argWithTypeSig = do
+argWithTypeSig :: Parser ASTMeta
+argWithTypeSig = dbg "argWithTypeSig" $
+  meta $ do
   arg' <- arg
-  symbol ":"
-  sig <- typeList
+  sig <- symbol ":" *> typeList
   pure $ ASTTypeSig { astType = sig, astTypeSigVar = arg'}
 
 -- 文字列のリテラルを読む
-strLit :: Parser AST
-strLit = do
+strLit :: Parser ASTMeta
+strLit = dbg "strLit" $
+  meta $ do
   beginChar <- single '"' <|> single '\''
   str <- many $ noneOf [beginChar]
   _   <- symboln [beginChar]
@@ -201,11 +204,10 @@ strLit = do
 
 -- リストのリテラルを読む
 listLit :: Parser ASTMeta
-listLit = do
-  srcPos <- getSourcePos
+listLit = dbg "listLit" $
+  meta $ do
   asts   <- brackets $ exprAst `sepBy` (symbol "," <|> symbol ";")
-  pure ASTMeta { srcPos = srcPos
-               , ast = ASTListLit { astList = asts } }
+  pure ASTListLit { astList = asts }
 
 -- -- カッコ式（カッコの中に式が一つだけある式）を読む
 -- parenAST :: Parser ASTMeta
@@ -217,11 +219,10 @@ listLit = do
 
 -- 複式（改行で区切られて連続する式）を読む
 seqAST :: Parser ASTMeta
-seqAST = do
-  srcPos <- getSourcePos
+seqAST = dbg "seqAST" $
+  meta $ do
   asts   <- braces $ many newLine *> many astNewLine
-  pure ASTMeta { srcPos = srcPos
-               , ast = ASTSeq { astSeq = asts } }
+  pure ASTSeq { astSeq = asts }
 
 -- 式を読む。後ろの改行の連続をスキップする
 astNewLine :: Parser ASTMeta
@@ -233,35 +234,33 @@ newLine = symbol "\n" <|> symbol ";"
 
 -- プログラムのトップレベルを読む
 topLevel :: Parser ASTMeta
-topLevel = do
-  srcPos <- getSourcePos
-  sc
+topLevel = dbg "topLevel" $
+  meta $ do
   asts <- many newLine *> some astNewLine
-  pure ASTMeta { srcPos = srcPos
-               , ast    = ASTSeq { astSeq = asts }}
+  pure ASTSeq { astSeq = asts }
 
 -- 型定義を読む
 typeDef :: Parser ASTMeta
-typeDef = do
-  srcPos <- getSourcePos
+typeDef = dbg "typeDef" $
+  meta $ do
   rword "type"
   name <- var identifier <* symbol "="
   types <- braces (memberWithType `sepBy1` symbol ",") <* many newLine
-  pure ASTMeta { srcPos = srcPos
-               , ast = ASTTypeDef { astTypeDefName   = name
-                                  , astTypeDefFields = types}}
+  pure ASTTypeDef { astTypeDefName   = name
+                  , astTypeDefFields = types}
 
 -- 型定義中の、構造体のメンバーとその型を読む
 memberWithType :: Parser (String, RecList Type)
-memberWithType = do
+memberWithType = dbg "memberWithType" $
+  do
   member <- identifier <* symbol ":"
   types  <- typeList
   pure (member, types)
 
 -- 関数定義を読む
 fundef :: Parser ASTMeta
-fundef = do
-  srcPos <- getSourcePos
+fundef = dbg "fundef" $
+  meta $ do
   rword "fun"
   nameAST <- try (var identifier) <|> var operator
   types'  <- optional (symbol ":" *> typeList) <* symbol "="
@@ -269,16 +268,15 @@ fundef = do
   body    <- exprAst
   -- 型を省略した場合はもっとも一般的な型にしちゃう
   let types = fromMaybe (makeGeneralType $ length params) types'
-  pure ASTMeta { srcPos = srcPos
-               , ast = ASTFunDef { astType       = types
-                                 , astFunDefName = nameAST
-                                 , astFunParams  = params
-                                 , astFunBody    = body }}
+  pure ASTFunDef { astType       = types
+                 , astFunDefName = nameAST
+                 , astFunParams  = params
+                 , astFunBody    = body }
 
 -- パターンマッチを含む関数定義を読む
 funDefCase :: Parser ASTMeta
-funDefCase = do
-  srcPos  <- getSourcePos
+funDefCase = dbg "funDefCase" $
+  meta $ do
   rword "fun"
   nameAST <- try (var identifier) <|> var operator
   types'  <- optional (symbol ":" *> typeList) <* symbol "="
@@ -287,13 +285,11 @@ funDefCase = do
             { astType        = types
             , astCaseBranchs = matches }
     }     <- braces $ many newLine *> caseAST types'
-  pure $ ASTMeta { srcPos = srcPos
-                 , ast = ASTFunDef
-                         { astType       = types
-                         , astFunDefName = nameAST
-                         , astFunParams  = paramList $ paramNum matches
-                         , astFunBody    = caseAST
-                         } }
+  pure ASTFunDef { astType       = types
+                 , astFunDefName = nameAST
+                 , astFunParams  = paramList $ paramNum matches
+                 , astFunBody    = caseAST
+                 }
 
 
 paramNum = length . fst3 . head
@@ -301,18 +297,18 @@ paramList n = zipWith (++) (replicate n "x") (map show (take n [1..]))
 fst3 (a,_,_) = a
 
 caseAST :: Maybe (RecList Type) -> Parser ASTMeta
-caseAST maybeType = do
-  srcPos  <- getSourcePos
+caseAST maybeType = dbg "caseAST" $
+  meta $ do
   matches <- some matchAST
   -- 型を省略した場合はもっとも一般的な型にしちゃう
   let types = fromMaybe (makeGeneralType (paramNum matches)) maybeType
-  pure ASTMeta { srcPos = srcPos
-               , ast    = ASTCase { astType        = types
-                                  , astCaseBranchs = matches }}
+  pure ASTCase { astType        = types
+               , astCaseBranchs = matches }
 
 -- パターンマッチ式を読む
 matchAST :: Parser ([ASTMeta], ASTMeta, Maybe ASTMeta)
-matchAST = do
+matchAST = dbg "matchAST" $
+  do
   conds <- some arg
   guard <- optional ( symbol "|" *> exprAst <* symbol "|") <* symbol "->"
   body <- exprAst <* many newLine
@@ -321,44 +317,46 @@ matchAST = do
 
 -- if式を読む
 ifAST :: Parser ASTMeta
-ifAST = do
-  srcPos <- getSourcePos
+ifAST = dbg "ifAST" $
+  meta $ do
   rword "if"
   condAST <- exprAst
   rword "then"
   thenAST <- exprAst
   rword "else"
   elseAST <- exprAst
-  pure $ ASTMeta { srcPos = srcPos
-                 , ast = ASTIf { astIfCond = condAST
-                               , astIfThen = thenAST
-                               , astIfElse = elseAST} }
+  pure ASTIf { astIfCond = condAST
+             , astIfThen = thenAST
+             , astIfElse = elseAST }
 
 -- 関数適用を読む
 apply :: Parser ASTMeta
-apply = do
-  srcPos <- getSourcePos
+apply = dbg "apply" $
+  meta $ do
   caller <- parens exprAst <|> var identifier
   args   <- some arg
-  pure ASTMeta { srcPos = srcPos
-               , ast = ASTApply caller args }
+  pure ASTApply { astApplyFun  = caller
+                , astApplyArgs = args}
 
 -- 型注釈を読む
-typeList :: Parser (RecList String)
-typeList = do
+typeList :: Parser (RecList Type)
+typeList = dbg "typeList" $
+  do
   term1 <- typeTerm
   terms <- many $ symbol "->" *> typeTerm
   pure $ Elems (term1 : terms)
 
 -- 型を表す項を読む。Int, a, [Double], (Int->String) など。
-typeTerm :: Parser (RecList String)
-typeTerm = try listTerm
+typeTerm :: Parser (RecList Type)
+typeTerm = dbg "typeTerm" $
+  try listTerm
   <|> (Elem <$> identifier)
   <|> parens typeList
 
 -- リスト型を読む
-listTerm :: Parser (RecList String)
-listTerm = do
+listTerm :: Parser (RecList Type)
+listTerm = dbg "listTerm" $
+  do
   term <- brackets identifier
   pure $ Elems [ Elem "List", Elem term ]
 
